91ab9c8b58e187d965b33da1448b1f1f53fdb6d2
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-10 00:17:27 +0100

    An update

--- DotNetWebDemo/obj/Debug/net9.0/DotNetWebDemo.AssemblyInfo.cs
+++ DotNetWebDemo/obj/Debug/net9.0/DotNetWebDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetWebDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801794fa398b90b57fa74d0f5c9f9e0fcfa65c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb1796737c55152f27a5647987a872b8ff1f4e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetWebDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetWebDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
