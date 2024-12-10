bf32707be0491838a98229c375c7de04631b5f51
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-10 09:05:02 +0100

    An update

--- DotNetWebDemo/Data/MenuContext.cs
+++ DotNetWebDemo/Data/MenuContext.cs
@@ -1,12 +1,33 @@
+using System.Diagnostics;
+using DotNetWebDemo.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetWebDemo.Data
 {
-    public class Context : DbContext
+    public class MenuContext : DbContext
     {
-        public MenuContext()
+        public MenuContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
             
         }  
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new {
+                di.DishId,
+                di.IngredientId
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.dishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.ingredient).WithMany(di => di.dishIngredients).HasForeignKey(d => d.IngredientId);
+            modelBuilder.Entity<Dish>().HasData(new Dish {Id =1 , Name="Margheritta" , price=7.82,ImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd6jONKGIT4J-tBAzmZsP0sP9PU7oVV2Oc7Q&s"});
+            modelBuilder.Entity<Ingredient>().HasData(new Ingredient {Id =1 , Name="Tomato Sauce"} , 
+                                                        new Ingredient {Id =2 , Name="Mozzarella"} );
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient{DishId =1 , IngredientId=1},
+                new DishIngredient{DishId =1 , IngredientId=2}
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<DishIngredient> DishIngredients{ get; set; }
+        public DbSet<Ingredient> Ingredients {get; set;}
     }
 }
--- DotNetWebDemo/Program.cs
+++ DotNetWebDemo/Program.cs
@@ -1,8 +1,13 @@
+using DotNetWebDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MenuContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
